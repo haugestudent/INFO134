a4986d3c2a96aaf15b3bbaba57017cefecf80130
runthele <31882357+runthele@users.noreply.github.com>
2018-03-16 15:28:36 +0100

    Update Googlemap.js
    Lagt til tabell over markers som skal vises med markert[] og også en testfunksjon for å fjerne markers etter nr. , slik at vi vil kunne tilpasse det som vises på kartet i forhold til på listen etter valg eller søk.

--- Googlemap.js
+++ Googlemap.js
@@ -1,47 +1,35 @@
-//xmlhttp = new XMLHttpRequest();
-//xmlhttp.open("GET",toalett.txt,false);
-//xmlhttp.send();
-//var text2 = xmlhttp.responseText;
-
 
 var pos = [];
-var senter = {lat: 60.394, lng: 5.321};
+var senter = {lat: 60.394, lng: 5.322};
 var sted = [];
-var obj = JSON.parse(text);
 var info = [];
-
-function initMap() {
-   var map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 13.8,
+var markert = [];
+for (i=0; i<obj.entries.length; i++) {
+	markert[i]=1;
+}
+	function initMap() {
+	var kart = new google.maps.Map(document.getElementById('map'), {
+          zoom: 14.3,
           center: senter
         });
 
 		for (i=0; i<obj.entries.length; i++) {
-			info[i] = "pris: " + obj.entries[i].pris.toString(),			// legger pris som en infoverdi, f.eks. for museklikk
-			sted[i] = {lat: obj.entries[i].lat, lng: obj.entries[i].long};	// legger koord i variabel så de kan benyttes senere også ved søk
-			pos [i] = new google.maps.Marker({
-			position: sted[i],
-			label: obj.entries[i].Id.toString() ,
-			map: map,
-			title: obj.entries[i].plassering + " / " + obj.entries[i].place	+ " (" +obj.entries[i].Adresse + ")" // setter tittel tilsvarende verdi i Id, vises ved mouseover
-			});
+			if (markert[i]==1) {
+				putOnMap(i);
+			}
+		}
+	
+	function putOnMap(i) {
+		
+		sted[i] = {lat: obj.entries[i].lat, lng: obj.entries[i].long};	// legger koord i variabel så de kan benyttes senere også ved søk
+		pos[i] = new google.maps.Marker({
+											position: sted[i],
+											label: obj.entries[i].Id.toString() ,
+											map: kart,
+											title: obj.entries[i].plassering + " / " + obj.entries[i].place	
+											// setter tittel tilsvarende verdi i Id, vises ved mouseover
+										}); // slutt på marker-objektet
+	//	pos[i].setMap(map);
+		if ((i<4)||(i>8)) {pos[i].setMap(null);}	// test for å fjerne markers fra kartet ##funker herfra, men ikke fra HTML eller separat.js
 	}
-}
-
-function visTid() {
-    var ukedag = ["ingen", "Mandag", "Tirsdag", "Onsdag", "Torsdag", "Fredag", "Lørdag", "Søndag"];
-	var today = new Date();
-    var h = today.getHours();
-    var m = today.getMinutes();
-    var s = today.getSeconds();
-	var d = today.getDay();
-    m = sjekkTid(m);
-    s = sjekkTid(s);
-    document.getElementById('klokke').innerHTML =
-    "Klokken er nå: " + h + ":" + m + ":" + s + " " + ukedag[d];
-    var t = setTimeout(visTid, 500);
-}
-function sjekkTid(i) {
-    if (i < 10) {i = "0" + i};  // legger 0 foran alle enkeltsiffer
-    return i;
 }
